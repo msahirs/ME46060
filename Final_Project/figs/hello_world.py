e5c117cf18a3aa3caa69cf3fcbbb820eb71ff32d
ssujahudeen <sujahudeen.sahir@gmail.com>
2023-06-24 14:11:43 +0200

    Added 3D truss solver

---
+++ Final_Project/figs/hello_world.py
@@ -0,0 +1,11 @@
+"Delete this file once content is added to parent directory"
+
+def print_hello():
+    
+    print("Hello World!\nPls delete this file")
+
+def main():
+
+    print_hello()
+
+if __name__ == '__main__': main()
